f5069a9dfe87a74850a7a4c60fcee8eeab8790d2
Adair <lon3w0lff06@outlook.com>
2026-05-10 20:04:22 -0600

    Implementación de modulo de conexión WiFi

---
+++ sistema/esp32/comunicacion/conexion_wifi.py
@@ -0,0 +1,27 @@
+#
+
+import network
+import time
+
+SSID = "RaspberryPi_SSID"
+PASSWORD = "RaspberryPi_Password"
+
+def conectar_wifi(ssid, password, intentos=5, delay=2):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    intento = 0
+    while not wlan.isconnected() and intento < intentos:
+        print(f"Intento {intento+1} para conectar...")
+        wlan.connect(ssid, password)
+        time.sleep(delay)
+        intento += 1
+    if wlan.isconnected():
+        print("Conectado:", wlan.ifconfig())
+        return True
+    else:
+        print("Fallo al conectar.")
+        return False
+
+def verificar_conexion():
+    wlan = network.WLAN(network.STA_IF)
+    return wlan.isconnected()
