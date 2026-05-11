1f051832455d9078a2b34d04b0eb99119e01a200
Adair <lon3w0lff06@outlook.com>
2026-05-10 20:58:40 -0600

    Implementada lógica no bloqueante en el ciclo principal

--- sistema/esp32/main.py
+++ sistema/esp32/main.py
@@ -1,17 +1,24 @@
 #
 
 import nucleo
+import utime
+
+ultima = utime.ticks_ms()
+intervalo = 50
 
 sm = MaquinaEstado()
 
 while True:
-    if sm.estado == nucleo.ESTADO_BOOT:
-        sm.boot(SSID, PASSWORD)
-    elif sm.estado == nucleo.ESTADO_ESPERA:
-        sm.espera(SSID, PASSWORD)
-    elif sm.estado == nucleo.ESTADO_OPERANDO:
-        sm.operando(SSID, PASSWORD)
-    elif sm.estado == nucleo.ESTADO_ERROR:
-        sm.error()
+    actual = utime.ticks_ms()
 
-    time.sleep(1)
+    if utime.ticks_diff(actual, ultima) >= intervalo:
+        ultima = actual
+
+        if sm.estado == nucleo.ESTADO_BOOT:
+            sm.boot(SSID, PASSWORD)
+        elif sm.estado == nucleo.ESTADO_ESPERA:
+            sm.espera(SSID, PASSWORD)
+        elif sm.estado == nucleo.ESTADO_OPERANDO:
+            sm.operando(SSID, PASSWORD)
+        elif sm.estado == nucleo.ESTADO_ERROR:
+            sm.error()
