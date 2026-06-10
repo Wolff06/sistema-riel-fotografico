c67591d4f0520e926b8ed0de82483fe176237661
arik36 <beckerastoria@gmail.com>
2026-06-10 13:18:16 -0600

    Add comments for code objective and contributors
    Added comments outlining the code's objective and contributors.

--- sistema/raspberry/interfaz/main_pantalla.py
+++ sistema/raspberry/interfaz/main_pantalla.py
@@ -1,3 +1,13 @@
+# - OBJETIVO DEL CODIGO -
+# Inicializar y ejecutar la interfaz
+# gráfica del sistema mediante la 
+# estructura Modelo-Vista-Controlador.
+
+# - INTREGANTES -
+# Macias Campos Ariadne Lizett
+# Soto Garnica Ari Adair
+# Lira Gamiño Luis Fernando
+
 from modelo_pantalla import ModeloPantalla
 from vista_pantalla import VistaPantalla
 from controlador_pantalla import ControladorPantalla
