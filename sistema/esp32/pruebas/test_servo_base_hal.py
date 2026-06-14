e69382e2d30cacff2188b3cae90b1be494dedd8c
arik36 <beckerastoria@gmail.com>
2026-06-13 19:15:11 -0600

    Integracion de integrantes

--- sistema/esp32/pruebas/test_servo_base_hal.py
+++ sistema/esp32/pruebas/test_servo_base_hal.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 # Prueba rapida del servo base usando la HAL del proyecto.
-
+# =============================================================================
+# PROYECTO: PROYECTO: CANMA - Garra Robotica con Camara e IA
+# - OBJETIVO DEL CODIGO -
+# Probar el movimiento del servomotor de la base de la garra/cámara recorriendo varios ángulos de forma automática.
+#
+# - INTREGANTES -
+# Macias Campos Ariadne Lizett
+# Soto Garnica Ari Adair
+# Lira Gamiño Luis Fernando
+#
+# =============================================================================
 import utime
 import hardware
 
