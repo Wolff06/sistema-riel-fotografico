e6a1a64e74ad6f8b2c63c73cf72982876d62d6b2
arik36 <beckerastoria@gmail.com>
2026-06-13 19:13:54 -0600

    Integracion de integrantes
    Added team member names to the file header.

--- sistema/esp32/nucleo/__init__.py
+++ sistema/esp32/nucleo/__init__.py
@@ -4,6 +4,11 @@
 # ARCHIVO:
 # sistema/esp32/nucleo/__init__.py
 #
+# - INTREGANTES -
+# Macias Campos Ariadne Lizett
+# Soto Garnica Ari Adair
+# Lira Gamiño Luis Fernando
+#
 # OBJETIVO:
 # Exponer la máquina de estados del ESP32 para que pueda importarse desde main.py.
 # =============================================================================
